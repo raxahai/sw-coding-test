4157be5a5e6ac343bf3d4cb5940a8300d8daaa62
Syed Raza Haider <97038058+syed-raza-haider@users.noreply.github.com>
2023-07-23 23:39:18 +0500

    Add app initialization point

---
+++ src/main/java/com/smallworld/TransactionalServiceApplication.java
@@ -0,0 +1,11 @@
+package com.smallworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalServiceApplication.class);
+    }
+}
