387519d199b9ea1a0b5a7df95d9b7a782c4d7b4e
Syed Raza Haider <97038058+syed-raza-haider@users.noreply.github.com>
2023-07-24 15:17:25 +0500

    Add service based exception

---
+++ src/main/java/com/smallworld/exception/ServiceException.java
@@ -0,0 +1,15 @@
+package com.smallworld.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ServiceException extends RuntimeException{
+    private String message;
+
+    public ServiceException(String message){
+        super(message);
+        this.message = message;
+    }
+}
